5849e3e64c35d22acfa51afb5c7cebe81675ad28
jacksors <jackson@jstone.us>
2021-01-26 00:15:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from collections import Counter
 from discord.ext import commands
 from bot_token import *
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix = '+', intents=intents)
 
