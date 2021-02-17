60710e77985cc433aab154e8c848e3e91912fa88
jacksors <jackson@jstone.us>
2021-02-16 23:23:29 -0600

    update main.py

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ async def help(ctx):
     embed.add_field(name='+setquoteschannel', value='Type this in the channel you wish to be your servers quotes channel. In order to run this command, you must have the role \"QuotesBot Admin\"', inline=False)
     embed.add_field(name='+delquoteschannel', value='Type this in the channel your quotes channel if you wish for it to no longer be a quotes channel. In order to run this command, you must have the role \"QuotesBot Admin\"', inline=False)
     embed.add_field(name='+togglementions', value='Toggles whether a the author of the quote is mentioned or not when randomquote and numquotes are run. On by default, but turn off to avoid excessive mentions in large servers. In order to run this command, you must have the role \"QuotesBot Admin\"', inline=False)
-    embed.add_field(name='Links', value='[Github](https://github.com/jacksors/Quotes-Discord-Bot) | [Support Server](https://discord.gg/DmYw7CbXfT)', inline=False)
+    embed.add_field(name='Links', value='[Github](https://github.com/jacksors/Quotes-Discord-Bot) | [Support Server](https://discord.gg/DmYw7CbXfT) | [Top.gg](https://top.gg/bot/799028695368073255)', inline=False)
     await ctx.send(embed=embed)
     
 client.run(BOT_TOKEN)
